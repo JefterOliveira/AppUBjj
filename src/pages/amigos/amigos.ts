ef0b8a632e54702cd8783037543e2196b83a61d7
Jefter Oliveira <jeftermoliveira@gmail.com>
2018-01-14 21:31:22 -0200

    tela de amigos

--- src/pages/amigos/amigos.ts
+++ src/pages/amigos/amigos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 
 /**
  * Generated class for the AmigosPage page.
@@ -15,11 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AmigosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl:ModalController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AmigosPage');
+  }
+  openModal(){
+    const modal= this.modalCtrl.create('ModalAddPage');
+    modal.present();
   }
 
 }
--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
 
 /**
- * Generated class for the ModalPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+* Generated class for the ModalPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
 
 @IonicPage()
 @Component({
@@ -14,17 +14,17 @@ import { IonicPage, NavController, NavParams, ViewController} from 'ionic-angula
   templateUrl: 'modal.html',
 })
 export class ModalPage {
-
+  
   constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController) {
   }
-
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad ModalPage');
   }
-
-    closeModal(){
-      this.view.dismiss();
-
-    }
-
+  
+  closeModal(){
+    this.view.dismiss();
+    
+  }
+  
 }
